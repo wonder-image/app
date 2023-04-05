8f84129aa103caf63b201a585e4077adfbe9734a
Andrea Marinoni <marinoni@wonderimage.it>
2023-04-05 15:55:41 +0200

    Update user.php

--- 0.0.1/build/row/user.php
+++ 0.0.1/build/row/user.php
@@ -3,11 +3,11 @@
     if (sqlSelect('user', ['username' => '@wonder'], 1)->Nrow == 0) {
         
         $values = [
-            "name" => sanitize("Andrea"),
-            "surname" => sanitize("Marinoni"),
-            "email" => sanitize("marinoni@wonderimage.it"),
-            "username" => sanitize("@wonder"),
-            "password" => hashPassword("admin"),
+            "name" => sanitize($_ENV['USER_NAME']),
+            "surname" => sanitize($_ENV['USER_SURNAME']),
+            "email" => sanitize($_ENV['USER_EMAIL']),
+            "username" => sanitize($_ENV['USER_USERNAME']),
+            "password" => hashPassword($_ENV['USER_PASSWORD']),
             "authority" => json_encode(["admin"]),
             "area" => json_encode(["backend"]),
             "active" => "true"
