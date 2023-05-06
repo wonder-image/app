572a37d073babac84718fc934a0abd1646b6c341
Andrea Marinoni <marinoni@wonderimage.it>
2023-05-06 09:01:30 +0200

    1.1.8

--- wonder-image.php
+++ wonder-image.php
@@ -15,7 +15,7 @@
 
     if (!isset($_SESSION['user_id'])) { $_SESSION['user_id'] = null; }
 
-    $APP_VERSION = "1.1.7";
+    $APP_VERSION = "1.1.8";
     $ROOT_APP = __DIR__."/$APP_VERSION";
 
     require_once $ROOT."/vendor/autoload.php";
