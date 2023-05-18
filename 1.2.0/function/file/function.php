b8658213f1d970b00fdbf40bf079b755978254d9
Andrea Marinoni <marinoni@wonderimage.it>
2023-05-18 11:19:27 +0200

    Nuova funzione getMimeType()
    Per riconoscere il tipo di file

--- 1.2.0/function/file/function.php
+++ 1.2.0/function/file/function.php
@@ -1,5 +1,6 @@
 <?php
 
+    require_once $ROOT_APP."/function/file/extension.php";
     require_once $ROOT_APP."/function/file/barcode.php";
     require_once $ROOT_APP."/function/file/qr.php";
     require_once $ROOT_APP."/function/file/resize.php";
