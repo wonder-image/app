6913fbac75706bd454a2eb5d411c9b353f9578e6
Andrea Marinoni <marinoni@wonderimage.it>
2023-05-30 14:16:51 +0200

    Delete Light Gallery

--- 1.2.0/utility/frontend/body-end.php
+++ 1.2.0/utility/frontend/body-end.php
@@ -7,21 +7,6 @@
     </div>
 </section>
 
-<!-- Light Gallery -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/lightgallery/2.7.1/plugins/autoplay/lg-autoplay.min.js" integrity="sha512-R+2bAdYZD7VnrK1YLwmprKNY+3gkX6UW3B58EXgb0sirtq+XdgY64ebDJDI/wi7USV1ZIF6sJk4uFto+fpKIXQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/lightgallery/2.7.1/plugins/comment/lg-comment.min.js" integrity="sha512-XgIhZz+Mff7Oc4l1Mam/LUuhRFmsXceX4GDsSNwG9i/Gbq8CyuBCFa6d2chfnJ7LvdPRpiy82NIKPEVoK8Drhw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/lightgallery/2.7.1/plugins/fullscreen/lg-fullscreen.min.js" integrity="sha512-wUl8rYJugCiHiMm1uyGDqcgkvwoY9paD9vLJzT3e4mwp46yB0cicFVcvzy8N6UpbtQyFlJDBzrQQ3BNL72w1+A==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/lightgallery/2.7.1/plugins/hash/lg-hash.min.js" integrity="sha512-pfR9czwvfE/w0qDuaWADXeq5mXvCmQKDni7xNK7m0KP6kbWZXRI2Ipn2YQqHK841UrrNzG4UIcr4qpvNH4Yo6A==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/lightgallery/2.7.1/plugins/mediumZoom/lg-medium-zoom.min.js" integrity="sha512-nMuTAVO/JX4krfPgOYQrnQrGknkoLXsvepoXIIRc2LHF8/PWTTxfaIm59GzMIvcLjkfbgPR6EsunU4gQcMC9Ng==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/lightgallery/2.7.1/plugins/pager/lg-pager.min.js" integrity="sha512-JuEDozFHrFXMEQ9WXhpLpfmdIZs53JlKO1v20E2GUVf/VzcgW/KIhHZYWFfjLewmNEPptVSfjNM1jzL0jh7tkg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/lightgallery/2.7.1/plugins/relativeCaption/lg-relative-caption.min.js" integrity="sha512-dxC6HeKyHS1xpRM/a6mbsdI3O22ycIuplRB9UG0tev23qmX0B2LkVqYfgyy8NoALgZMvDsasc3VUEUg2CXIwrg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/lightgallery/2.7.1/plugins/rotate/lg-rotate.min.js" integrity="sha512-5RFEmyJMZJEfPcXbVnwKA8SwcS5UqBRBPqpmDr4j37mdR6W2g0oCgnkAnTl6rtTfCumDjyG/A+NT0hPo9qsnpA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/lightgallery/2.7.1/plugins/share/lg-share.min.js" integrity="sha512-d4/o0e86InPByL5BxWmT/HPwxDeLaWU41vYRG48FN/hO964JyTaNAjnI6/CZyz2czEbpVm4iQhsBjmJMksieDw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/lightgallery/2.7.1/plugins/thumbnail/lg-thumbnail.min.js" integrity="sha512-Jx+orEb1KJtJ6Ajfshhr7is0zqgUC7H9ylk76KMtB9Ea2WAf/Muyzpe9zvBAYQQQKdAbj+rNYEorsRQLsmRafA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/lightgallery/2.7.1/plugins/video/lg-video.min.js" integrity="sha512-mjdf6tU1Mksu9Hq2YXfbxdzzYHU7SJYmAsMnrhBf80SkHFvk6eHa2d79JM0q5w5ft5nQyBQ0EMB+XTmnvhcFgA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/lightgallery/2.7.1/plugins/vimeoThumbnail/lg-vimeo-thumbnail.min.js" integrity="sha512-etFE2LIqmbwAGacj3GhhXJCWX3idM39n+bX3O0rJDCFqG5+jqpmP0qcy192g9S+1/Q0fJ+SpbfTsdrekm33IeQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/lightgallery/2.7.1/plugins/zoom/lg-zoom.min.js" integrity="sha512-BfC/MaayF9sOZyn1bs1R1P8dEugU7v0j5Qu2FeWVfF/rhKUKZBD9kgNqRNinefIp9zAE7g2KhlwwhMpl5V1jMA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-
 <!-- Swiper.js -->
 <script src="https://cdn.jsdelivr.net/npm/swiper@9/swiper-element-bundle.min.js"></script>
 
