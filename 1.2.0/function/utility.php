331e5c1107a1a850d4371698ddccf881b160fa4e
Andrea Marinoni <marinoni@wonderimage.it>
2023-06-12 18:07:00 +0200

    Update utility.php

--- 1.2.0/function/utility.php
+++ 1.2.0/function/utility.php
@@ -11,10 +11,10 @@
         } else {
 
             $RETURN = (object) array();
-            $RETURN->color = $array['$key']['color'];
-            $RETURN->name = $array['$key']['name'];
-            $RETURN->text = $array['$key']['text'];
-            $RETURN->classIcon = $array['$key']['icon'];
+            $RETURN->color = $array[$key]['color'];
+            $RETURN->name = $array[$key]['name'];
+            $RETURN->text = $array[$key]['text'];
+            $RETURN->classIcon = $array[$key]['icon'];
 
             $RETURN->icon = "<i class='$RETURN->classIcon'></i>";
             $RETURN->tooltip = "<i class='$RETURN->classIcon' data-bs-toggle='tooltip' data-bs-placement='top' data-bs-title='$RETURN->text'></i>";
