1a8d5cc6eb56496f56ccb91af6eab71904e854e6
Andrea Marinoni <marinoni@wonderimage.it>
2023-08-30 18:20:50 +0200

    Statistiche

--- 1.2.2/generator/stats/index.php
+++ 1.2.2/generator/stats/index.php
@@ -7,98 +7,102 @@
     $ROOT = $_SERVER['DOCUMENT_ROOT'];
     require_once $ROOT."/vendor/wonder-image/app/wonder-image.php";
 
-    $mysqli = $MYSQLI_CONNECTION['stats'];
+    if (is_array($DB->database) && array_key_exists('stats', $DB->database)) {
+            
+        $mysqli = $MYSQLI_CONNECTION['stats'];
 
-    if ($_GET['frequency'] == 'hourly') {
-        
-        $TB_VR = "visitors_recap_HBH";
-        $TB_URI_VR = "uri_vr_HBH";
-        $TB_URL_VR = "url_vr_HBH";
-        $TB_PAGE_TITLE_VR = "page_title_VR_HBH";
+        if ($_GET['frequency'] == 'hourly') {
+            
+            $TB_VR = "visitors_recap_hbh";
+            $TB_URI_VR = "uri_vr_hbh";
+            $TB_URL_VR = "url_vr_hbh";
+            $TB_PAGE_TITLE_VR = "page_title_vr_hbh";
 
-        $FROM = date('Y-m-d H', strtotime('-1 hours')).':00:00';
-        $TO = date('Y-m-d H').':00:00';
+            $FROM = date('Y-m-d H', strtotime('-1 hours')).':00:00';
+            $TO = date('Y-m-d H').':00:00';
 
-    } elseif ($_GET['frequency'] == 'daily') {
-        
-        $TB_VR = "visitors_recap_DBD";
-        $TB_URI_VR = "uri_vr_DBD";
-        $TB_URL_VR = "url_vr_DBD";
-        $TB_PAGE_TITLE_VR = "page_title_VR_DBD";
+        } elseif ($_GET['frequency'] == 'daily') {
+            
+            $TB_VR = "visitors_recap_dbd";
+            $TB_URI_VR = "uri_vr_dbd";
+            $TB_URL_VR = "url_vr_dbd";
+            $TB_PAGE_TITLE_VR = "page_title_vr_dbd";
 
-        $FROM = date('Y-m-d', strtotime('-1 day')).' 00:00:00';
-        $TO = date('Y-m-d', strtotime('-1 day')).' 23:59:59';
+            $FROM = date('Y-m-d', strtotime('-1 day')).' 00:00:00';
+            $TO = date('Y-m-d', strtotime('-1 day')).' 23:59:59';
 
-    } elseif ($_GET['frequency'] == 'monthly') {
-        
-        $TB_VR = "visitors_recap_MBM";
-        $TB_URI_VR = "uri_vr_MBM";
-        $TB_URL_VR = "url_vr_MBM";
-        $TB_PAGE_TITLE_VR = "page_title_VR_MBM";
+        } elseif ($_GET['frequency'] == 'monthly') {
+            
+            $TB_VR = "visitors_recap_mbm";
+            $TB_URI_VR = "uri_vr_mbm";
+            $TB_URL_VR = "url_vr_mbm";
+            $TB_PAGE_TITLE_VR = "page_title_vr_mbm";
 
-        $FROM = date('Y-m-', strtotime('-1 day')).'01 00:00:00';
-        $TO = date('Y-m-d', strtotime('-1 day')).' 23:59:59';
+            $FROM = date('Y-m-', strtotime('-1 day')).'01 00:00:00';
+            $TO = date('Y-m-d', strtotime('-1 day')).' 23:59:59';
 
-    }
+        }
 
-    $VISITORS_RECAP = [
-        "visitors" => sqlSelect('visitors_log', "`creation` BETWEEN '$FROM' AND '$TO'")->Nrow,
-        "sessions" => sqlSelect('visitors_log', "`creation` BETWEEN '$FROM' AND '$TO'", null, null, null, "DISTINCT session_id")->Nrow,
-        "visitors_unique" => sqlSelect('visitors_log', "`creation` BETWEEN '$FROM' AND '$TO'", null, null, null, "DISTINCT visitor_id")->Nrow,
-        "registered_users" => sqlSelect('visitors_log', "`creation` BETWEEN '$FROM' AND '$TO' AND `registered_user` = 'true'")->Nrow,
-        "https" => sqlSelect('visitors_log', "`creation` BETWEEN '$FROM' AND '$TO' AND `https` = 'on'")->Nrow
-    ];
-
-    sqlInsert($TB_VR, $VISITORS_RECAP);
-
-    foreach (sqlSelect('visitors_log', "`creation` BETWEEN '$FROM' AND '$TO'", null, null, null, "DISTINCT request_uri")->row as $key => $row) {
-
-        $request_uri = $row['request_uri'];
-
-        $URI_VR = [
-            "uri" => $request_uri,
-            "visitors" => sqlSelect('visitors_log', "`request_uri` = '$request_uri' AND `creation` BETWEEN '$FROM' AND '$TO'")->Nrow,
-            "sessions" => sqlSelect('visitors_log', "`request_uri` = '$request_uri' AND `creation` BETWEEN '$FROM' AND '$TO'", null, null, null, "DISTINCT session_id")->Nrow,
-            "visitors_unique" => sqlSelect('visitors_log', "`request_uri` = '$request_uri' AND `creation` BETWEEN '$FROM' AND '$TO'", null, null, null, "DISTINCT visitor_id")->Nrow,
-            "registered_users" => sqlSelect('visitors_log', "`request_uri` = '$request_uri' AND `creation` BETWEEN '$FROM' AND '$TO' AND `registered_user` = 'true'")->Nrow,
-            "https" => sqlSelect('visitors_log', "`request_uri` = '$request_uri' AND `creation` BETWEEN '$FROM' AND '$TO' AND `https` = 'on'")->Nrow
+        $VISITORS_RECAP = [
+            "visitors" => sqlSelect('visitors_log', "`creation` BETWEEN '$FROM' AND '$TO'")->Nrow,
+            "sessions" => sqlSelect('visitors_log', "`creation` BETWEEN '$FROM' AND '$TO'", null, null, null, "DISTINCT session_id")->Nrow,
+            "visitors_unique" => sqlSelect('visitors_log', "`creation` BETWEEN '$FROM' AND '$TO'", null, null, null, "DISTINCT visitor_id")->Nrow,
+            "registered_users" => sqlSelect('visitors_log', "`creation` BETWEEN '$FROM' AND '$TO' AND `registered_user` = 'true'")->Nrow,
+            "https" => sqlSelect('visitors_log', "`creation` BETWEEN '$FROM' AND '$TO' AND `https` = 'on'")->Nrow
         ];
 
-        sqlInsert($TB_URI_VR, $URI_VR);
+        sqlInsert($TB_VR, $VISITORS_RECAP);
 
-    }
+        foreach (sqlSelect('visitors_log', "`creation` BETWEEN '$FROM' AND '$TO'", null, null, null, "DISTINCT request_uri")->row as $key => $row) {
 
-    foreach (sqlSelect('visitors_log', "`creation` BETWEEN '$FROM' AND '$TO'", null, null, null, "DISTINCT script_url")->row as $key => $row) {
+            $request_uri = $row['request_uri'];
 
-        $script_url = $row['script_url'];
+            $URI_VR = [
+                "uri" => $request_uri,
+                "visitors" => sqlSelect('visitors_log', "`request_uri` = '$request_uri' AND `creation` BETWEEN '$FROM' AND '$TO'")->Nrow,
+                "sessions" => sqlSelect('visitors_log', "`request_uri` = '$request_uri' AND `creation` BETWEEN '$FROM' AND '$TO'", null, null, null, "DISTINCT session_id")->Nrow,
+                "visitors_unique" => sqlSelect('visitors_log', "`request_uri` = '$request_uri' AND `creation` BETWEEN '$FROM' AND '$TO'", null, null, null, "DISTINCT visitor_id")->Nrow,
+                "registered_users" => sqlSelect('visitors_log', "`request_uri` = '$request_uri' AND `creation` BETWEEN '$FROM' AND '$TO' AND `registered_user` = 'true'")->Nrow,
+                "https" => sqlSelect('visitors_log', "`request_uri` = '$request_uri' AND `creation` BETWEEN '$FROM' AND '$TO' AND `https` = 'on'")->Nrow
+            ];
 
-        $URL_VR = [
-            "url" => $script_url,
-            "visitors" => sqlSelect('visitors_log', "`script_url` = '$script_url' AND `creation` BETWEEN '$FROM' AND '$TO'")->Nrow,
-            "sessions" => sqlSelect('visitors_log', "`script_url` = '$script_url' AND `creation` BETWEEN '$FROM' AND '$TO'", null, null, null, "DISTINCT session_id")->Nrow,
-            "visitors_unique" => sqlSelect('visitors_log', "`script_url` = '$script_url' AND `creation` BETWEEN '$FROM' AND '$TO'", null, null, null, "DISTINCT visitor_id")->Nrow,
-            "registered_users" => sqlSelect('visitors_log', "`script_url` = '$script_url' AND `creation` BETWEEN '$FROM' AND '$TO' AND `registered_user` = 'true'")->Nrow,
-            "https" => sqlSelect('visitors_log', "`script_url` = '$script_url' AND `creation` BETWEEN '$FROM' AND '$TO' AND `https` = 'on'")->Nrow
-        ];
+            sqlInsert($TB_URI_VR, $URI_VR);
 
-        sqlInsert($TB_URL_VR, $URL_VR);
+        }
 
-    }
+        foreach (sqlSelect('visitors_log', "`creation` BETWEEN '$FROM' AND '$TO'", null, null, null, "DISTINCT script_url")->row as $key => $row) {
 
-    foreach (sqlSelect('visitors_log', "`creation` BETWEEN '$FROM' AND '$TO'", null, null, null, "DISTINCT page_title")->row as $key => $row) {
+            $script_url = $row['script_url'];
 
-        $page_title = $row['page_title'];
+            $URL_VR = [
+                "url" => $script_url,
+                "visitors" => sqlSelect('visitors_log', "`script_url` = '$script_url' AND `creation` BETWEEN '$FROM' AND '$TO'")->Nrow,
+                "sessions" => sqlSelect('visitors_log', "`script_url` = '$script_url' AND `creation` BETWEEN '$FROM' AND '$TO'", null, null, null, "DISTINCT session_id")->Nrow,
+                "visitors_unique" => sqlSelect('visitors_log', "`script_url` = '$script_url' AND `creation` BETWEEN '$FROM' AND '$TO'", null, null, null, "DISTINCT visitor_id")->Nrow,
+                "registered_users" => sqlSelect('visitors_log', "`script_url` = '$script_url' AND `creation` BETWEEN '$FROM' AND '$TO' AND `registered_user` = 'true'")->Nrow,
+                "https" => sqlSelect('visitors_log', "`script_url` = '$script_url' AND `creation` BETWEEN '$FROM' AND '$TO' AND `https` = 'on'")->Nrow
+            ];
 
-        $PAGE_TITLE_VR = [
-            "page_title" => $page_title,
-            "visitors" => sqlSelect('visitors_log', "`page_title` = '$page_title' AND `creation` BETWEEN '$FROM' AND '$TO'")->Nrow,
-            "sessions" => sqlSelect('visitors_log', "`page_title` = '$page_title' AND `creation` BETWEEN '$FROM' AND '$TO'", null, null, null, "DISTINCT session_id")->Nrow,
-            "visitors_unique" => sqlSelect('visitors_log', "`page_title` = '$page_title' AND `creation` BETWEEN '$FROM' AND '$TO'", null, null, null, "DISTINCT visitor_id")->Nrow,
-            "registered_users" => sqlSelect('visitors_log', "`page_title` = '$page_title' AND `creation` BETWEEN '$FROM' AND '$TO' AND `registered_user` = 'true'")->Nrow,
-            "https" => sqlSelect('visitors_log', "`page_title` = '$page_title' AND `creation` BETWEEN '$FROM' AND '$TO' AND `https` = 'on'")->Nrow
-        ];
+            sqlInsert($TB_URL_VR, $URL_VR);
 
-        sqlInsert($TB_PAGE_TITLE_VR, $PAGE_TITLE_VR);
+        }
+
+        foreach (sqlSelect('visitors_log', "`creation` BETWEEN '$FROM' AND '$TO'", null, null, null, "DISTINCT page_title")->row as $key => $row) {
+
+            $page_title = $row['page_title'];
+
+            $PAGE_TITLE_VR = [
+                "page_title" => $page_title,
+                "visitors" => sqlSelect('visitors_log', "`page_title` = '$page_title' AND `creation` BETWEEN '$FROM' AND '$TO'")->Nrow,
+                "sessions" => sqlSelect('visitors_log', "`page_title` = '$page_title' AND `creation` BETWEEN '$FROM' AND '$TO'", null, null, null, "DISTINCT session_id")->Nrow,
+                "visitors_unique" => sqlSelect('visitors_log', "`page_title` = '$page_title' AND `creation` BETWEEN '$FROM' AND '$TO'", null, null, null, "DISTINCT visitor_id")->Nrow,
+                "registered_users" => sqlSelect('visitors_log', "`page_title` = '$page_title' AND `creation` BETWEEN '$FROM' AND '$TO' AND `registered_user` = 'true'")->Nrow,
+                "https" => sqlSelect('visitors_log', "`page_title` = '$page_title' AND `creation` BETWEEN '$FROM' AND '$TO' AND `https` = 'on'")->Nrow
+            ];
+
+            sqlInsert($TB_PAGE_TITLE_VR, $PAGE_TITLE_VR);
+
+        }
 
     }
 
--- 1.2.2/utility/frontend/head.php
+++ 1.2.2/utility/frontend/head.php
@@ -38,61 +38,64 @@
         // 
 
         $SESSION_ID = session_id();
-
-        $VALUES = [
-            "visitor_id" => $VISITOR_ID,
-            "session_id" => $SESSION_ID,
-            "registered_user" => $REGISTERED_USER,
-            "user_id" => $USER_ID,
-            "page_title" => $SEO->title,
-            "path" => isset($_SERVER['PATH']) ? $_SERVER['PATH'] : '',
-            "oracle_home" => isset($_SERVER['ORACLE_HOME']) ? $_SERVER['ORACLE_HOME'] : '',
-            "http_accept" => isset($_SERVER['HTTP_ACCEPT']) ? $_SERVER['HTTP_ACCEPT'] : '',
-            "http_accept_encoding" => isset($_SERVER['HTTP_ACCEPT_ENCODING']) ? $_SERVER['HTTP_ACCEPT_ENCODING'] : '',
-            "http_accept_language" => isset($_SERVER['HTTP_ACCEPT_LANGUAGE']) ? $_SERVER['HTTP_ACCEPT_LANGUAGE'] : '',
-            "http_cookie" => isset($_SERVER['HTTP_COOKIE']) ? $_SERVER['HTTP_COOKIE'] : '',
-            "http_host" => isset($_SERVER['HTTP_HOST']) ? $_SERVER['HTTP_HOST'] : '',
-            "http_user_agent" => isset($_SERVER['HTTP_USER_AGENT']) ? $_SERVER['HTTP_USER_AGENT'] : '',
-            "http_priority" => isset($_SERVER['HTTP_PRIORITY']) ? $_SERVER['HTTP_PRIORITY'] : '',
-            "http_dnt" => isset($_SERVER['HTTP_DNT']) ? $_SERVER['HTTP_DNT'] : '',
-            "http_upgrade_insicure_requests" => isset($_SERVER['HTTP_UPGRADE_INSECURE_REQUESTS']) ? $_SERVER['HTTP_UPGRADE_INSECURE_REQUESTS'] : '',
-            "http_sec_fetch_dest" => isset($_SERVER['HTTP_SEC_FETCH_DEST']) ? $_SERVER['HTTP_SEC_FETCH_DEST'] : '',
-            "http_sec_fetch_mode" => isset($_SERVER['HTTP_SEC_FETCH_MODE']) ? $_SERVER['HTTP_SEC_FETCH_MODE'] : '',
-            "http_sec_fetch_site" => isset($_SERVER['HTTP_SEC_FETCH_SITE']) ? $_SERVER['HTTP_SEC_FETCH_SITE'] : '',
-            "http_sec_fetch_user" => isset($_SERVER['HTTP_SEC_FETCH_USER']) ? $_SERVER['HTTP_SEC_FETCH_USER'] : '',
-            "document_root" => isset($_SERVER['DOCUMENT_ROOT']) ? $_SERVER['DOCUMENT_ROOT'] : '',
-            "remote_addr" => isset($_SERVER['REMOTE_ADDR']) ? $_SERVER['REMOTE_ADDR'] : '',
-            "remote_port" => isset($_SERVER['REMOTE_PORT']) ? $_SERVER['REMOTE_PORT'] : '',
-            "server_addr" => isset($_SERVER['SERVER_ADDR']) ? $_SERVER['SERVER_ADDR'] : '',
-            "server_name" => isset($_SERVER['SERVER_NAME']) ? $_SERVER['SERVER_NAME'] : '',
-            "server_admin" => isset($_SERVER['SERVER_ADMIN']) ? $_SERVER['SERVER_ADMIN'] : '',
-            "server_port" => isset($_SERVER['SERVER_PORT']) ? $_SERVER['SERVER_PORT'] : '',
-            "request_scheme" => isset($_SERVER['REQUEST_SCHEME']) ? $_SERVER['REQUEST_SCHEME'] : '',
-            "request_uri" => isset($_SERVER['REQUEST_URI']) ? $_SERVER['REQUEST_URI'] : '',
-            "https" => isset($_SERVER['HTTPS']) ? $_SERVER['HTTPS'] : '',
-            "x_spdy" => isset($_SERVER['X_SPDY']) ? $_SERVER['X_SPDY'] : '',
-            "ssl_protocol" => isset($_SERVER['SSL_PROTOCOL']) ? $_SERVER['SSL_PROTOCOL'] : '',
-            "ssl_cipher" => isset($_SERVER['SSL_CIPHER']) ? $_SERVER['SSL_CIPHER'] : '',
-            "ssl_cipher_usekeysize" => isset($_SERVER['SSL_CIPHER_USEKEYSIZE']) ? $_SERVER['SSL_CIPHER_USEKEYSIZE'] : '',
-            "ssl_cipher_algkeysize" => isset($_SERVER['SSL_CIPHER_ALGKEYSIZE']) ? $_SERVER['SSL_CIPHER_ALGKEYSIZE'] : '',
-            "script_filename" => isset($_SERVER['SCRIPT_FILENAME']) ? $_SERVER['SCRIPT_FILENAME'] : '',
-            "query_string" => isset($_SERVER['QUERY_STRING']) ? $_SERVER['QUERY_STRING'] : '',
-            "script_uri" => isset($_SERVER['SCRIPT_URI']) ? $_SERVER['SCRIPT_URI'] : '',
-            "script_url" => isset($_SERVER['SCRIPT_URL']) ? $_SERVER['SCRIPT_URL'] : '',
-            "script_name" => isset($_SERVER['SCRIPT_NAME']) ? $_SERVER['SCRIPT_NAME'] : '',
-            "server_protocol" => isset($_SERVER['SERVER_PROTOCOL']) ? $_SERVER['SERVER_PROTOCOL'] : '',
-            "server_software" => isset($_SERVER['SERVER_SOFTWARE']) ? $_SERVER['SERVER_SOFTWARE'] : '',
-            "request_method" => isset($_SERVER['REQUEST_METHOD']) ? $_SERVER['REQUEST_METHOD'] : '',
-            "x_lschache" => isset($_SERVER['X-LSCACHE']) ? $_SERVER['X-LSCACHE'] : '',
-            "php_self" => isset($_SERVER['PHP_SELF']) ? $_SERVER['PHP_SELF'] : '',
-            "request_time_float" => isset($_SERVER['REQUEST_TIME_FLOAT']) ? $_SERVER['REQUEST_TIME_FLOAT'] : '',
-            "request_time" => isset($_SERVER['REQUEST_TIME']) ? $_SERVER['REQUEST_TIME'] : ''
-        ];
-
         
-        $mysqli = $MYSQLI_CONNECTION['stats'];
-        sqlInsert("visitors_log", $VALUES);
-        $mysqli = $MYSQLI_CONNECTION['main'];
+        if (is_array($DB->database) && array_key_exists('stats', $DB->database)) {
+
+            $VALUES = [
+                "visitor_id" => $VISITOR_ID,
+                "session_id" => $SESSION_ID,
+                "registered_user" => $REGISTERED_USER,
+                "user_id" => $USER_ID,
+                "page_title" => $SEO->title,
+                "path" => isset($_SERVER['PATH']) ? $_SERVER['PATH'] : '',
+                "oracle_home" => isset($_SERVER['ORACLE_HOME']) ? $_SERVER['ORACLE_HOME'] : '',
+                "http_accept" => isset($_SERVER['HTTP_ACCEPT']) ? $_SERVER['HTTP_ACCEPT'] : '',
+                "http_accept_encoding" => isset($_SERVER['HTTP_ACCEPT_ENCODING']) ? $_SERVER['HTTP_ACCEPT_ENCODING'] : '',
+                "http_accept_language" => isset($_SERVER['HTTP_ACCEPT_LANGUAGE']) ? $_SERVER['HTTP_ACCEPT_LANGUAGE'] : '',
+                "http_cookie" => isset($_SERVER['HTTP_COOKIE']) ? $_SERVER['HTTP_COOKIE'] : '',
+                "http_host" => isset($_SERVER['HTTP_HOST']) ? $_SERVER['HTTP_HOST'] : '',
+                "http_user_agent" => isset($_SERVER['HTTP_USER_AGENT']) ? $_SERVER['HTTP_USER_AGENT'] : '',
+                "http_priority" => isset($_SERVER['HTTP_PRIORITY']) ? $_SERVER['HTTP_PRIORITY'] : '',
+                "http_dnt" => isset($_SERVER['HTTP_DNT']) ? $_SERVER['HTTP_DNT'] : '',
+                "http_upgrade_insicure_requests" => isset($_SERVER['HTTP_UPGRADE_INSECURE_REQUESTS']) ? $_SERVER['HTTP_UPGRADE_INSECURE_REQUESTS'] : '',
+                "http_sec_fetch_dest" => isset($_SERVER['HTTP_SEC_FETCH_DEST']) ? $_SERVER['HTTP_SEC_FETCH_DEST'] : '',
+                "http_sec_fetch_mode" => isset($_SERVER['HTTP_SEC_FETCH_MODE']) ? $_SERVER['HTTP_SEC_FETCH_MODE'] : '',
+                "http_sec_fetch_site" => isset($_SERVER['HTTP_SEC_FETCH_SITE']) ? $_SERVER['HTTP_SEC_FETCH_SITE'] : '',
+                "http_sec_fetch_user" => isset($_SERVER['HTTP_SEC_FETCH_USER']) ? $_SERVER['HTTP_SEC_FETCH_USER'] : '',
+                "document_root" => isset($_SERVER['DOCUMENT_ROOT']) ? $_SERVER['DOCUMENT_ROOT'] : '',
+                "remote_addr" => isset($_SERVER['REMOTE_ADDR']) ? $_SERVER['REMOTE_ADDR'] : '',
+                "remote_port" => isset($_SERVER['REMOTE_PORT']) ? $_SERVER['REMOTE_PORT'] : '',
+                "server_addr" => isset($_SERVER['SERVER_ADDR']) ? $_SERVER['SERVER_ADDR'] : '',
+                "server_name" => isset($_SERVER['SERVER_NAME']) ? $_SERVER['SERVER_NAME'] : '',
+                "server_admin" => isset($_SERVER['SERVER_ADMIN']) ? $_SERVER['SERVER_ADMIN'] : '',
+                "server_port" => isset($_SERVER['SERVER_PORT']) ? $_SERVER['SERVER_PORT'] : '',
+                "request_scheme" => isset($_SERVER['REQUEST_SCHEME']) ? $_SERVER['REQUEST_SCHEME'] : '',
+                "request_uri" => isset($_SERVER['REQUEST_URI']) ? $_SERVER['REQUEST_URI'] : '',
+                "https" => isset($_SERVER['HTTPS']) ? $_SERVER['HTTPS'] : '',
+                "x_spdy" => isset($_SERVER['X_SPDY']) ? $_SERVER['X_SPDY'] : '',
+                "ssl_protocol" => isset($_SERVER['SSL_PROTOCOL']) ? $_SERVER['SSL_PROTOCOL'] : '',
+                "ssl_cipher" => isset($_SERVER['SSL_CIPHER']) ? $_SERVER['SSL_CIPHER'] : '',
+                "ssl_cipher_usekeysize" => isset($_SERVER['SSL_CIPHER_USEKEYSIZE']) ? $_SERVER['SSL_CIPHER_USEKEYSIZE'] : '',
+                "ssl_cipher_algkeysize" => isset($_SERVER['SSL_CIPHER_ALGKEYSIZE']) ? $_SERVER['SSL_CIPHER_ALGKEYSIZE'] : '',
+                "script_filename" => isset($_SERVER['SCRIPT_FILENAME']) ? $_SERVER['SCRIPT_FILENAME'] : '',
+                "query_string" => isset($_SERVER['QUERY_STRING']) ? $_SERVER['QUERY_STRING'] : '',
+                "script_uri" => isset($_SERVER['SCRIPT_URI']) ? $_SERVER['SCRIPT_URI'] : '',
+                "script_url" => isset($_SERVER['SCRIPT_URL']) ? $_SERVER['SCRIPT_URL'] : '',
+                "script_name" => isset($_SERVER['SCRIPT_NAME']) ? $_SERVER['SCRIPT_NAME'] : '',
+                "server_protocol" => isset($_SERVER['SERVER_PROTOCOL']) ? $_SERVER['SERVER_PROTOCOL'] : '',
+                "server_software" => isset($_SERVER['SERVER_SOFTWARE']) ? $_SERVER['SERVER_SOFTWARE'] : '',
+                "request_method" => isset($_SERVER['REQUEST_METHOD']) ? $_SERVER['REQUEST_METHOD'] : '',
+                "x_lschache" => isset($_SERVER['X-LSCACHE']) ? $_SERVER['X-LSCACHE'] : '',
+                "php_self" => isset($_SERVER['PHP_SELF']) ? $_SERVER['PHP_SELF'] : '',
+                "request_time_float" => isset($_SERVER['REQUEST_TIME_FLOAT']) ? $_SERVER['REQUEST_TIME_FLOAT'] : '',
+                "request_time" => isset($_SERVER['REQUEST_TIME']) ? $_SERVER['REQUEST_TIME'] : ''
+            ];
+            
+            $mysqli = $MYSQLI_CONNECTION['stats'];
+            sqlInsert("visitors_log", $VALUES);
+            $mysqli = $MYSQLI_CONNECTION['main'];
+        
+        }
 
     //
 
