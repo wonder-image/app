66ae6ee9b38dcdc432864ff2faded477d2ef517f
Andrea Marinoni <marinoni@wonderimage.it>
2023-10-24 12:52:38 +0200

    Update utility.php

--- 1.2.6/function/utility.php
+++ 1.2.6/function/utility.php
@@ -21,10 +21,10 @@
 
             $RETURN->icon = "<i class='$RETURN->classIcon'></i>";
             $RETURN->tooltip = "<i class='$RETURN->classIcon' data-bs-toggle='tooltip' data-bs-placement='top' data-bs-title='$RETURN->text'></i>";
-            $RETURN->badge = "<span class='badge text-bg-$RETURN->color'>$RETURN->name</span>";
+            $RETURN->badge = "<span class='badge text-bg-$RETURN->color'>".strtoupper($RETURN->name)."</span>";
             $RETURN->badgeTooltip = "<span class='badge text-bg-$RETURN->color' data-bs-toggle='tooltip' data-bs-placement='top' data-bs-title='$RETURN->text'>$RETURN->icon</span>";
             $RETURN->badgeIcon = "<span class='badge text-bg-$RETURN->color'>$RETURN->icon</span>";
-            $RETURN->automaticResize = "<span class='badge text-bg-$RETURN->color'><span class='pc-none'>$RETURN->icon</span><span class='phone-none'>$RETURN->name</span></span>";
+            $RETURN->automaticResize = "<span class='badge text-bg-$RETURN->color'><span class='pc-none'>$RETURN->icon</span><span class='phone-none'>".strtoupper($RETURN->name)."</span></span>";
 
         }
 
