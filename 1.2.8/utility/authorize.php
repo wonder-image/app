658df8b85eb41afaddd4cbe5eb24d6665117cf3f
Andrea Marinoni <marinoni@wonderimage.it>
2023-11-02 18:32:46 +0100

    Update authorize.php

--- 1.2.8/utility/authorize.php
+++ 1.2.8/utility/authorize.php
@@ -6,6 +6,8 @@
 
             if (empty($_POST)) {
                 $USER = authorizeUser('backend', $PERMIT, isset($_SESSION['user_id']) ? $_SESSION['user_id'] : null);
+            } else {
+                $USER = infoUser($_SESSION['user_id']);
             }
 
         } elseif (isset($FRONTEND) && $FRONTEND) {
