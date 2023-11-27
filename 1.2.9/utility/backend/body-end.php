bf1ec7424636761abbb76e7cafb5c70ff97ff70c
Andrea Marinoni <marinoni@wonderimage.it>
2023-11-27 18:30:16 +0100

    Loading spinner

--- 1.2.9/utility/backend/body-end.php
+++ 1.2.9/utility/backend/body-end.php
@@ -1,14 +1,3 @@
-<!-- Progress bar -->
-<div id="loading-spinner" class="position-fixed container-fluid bg-dark bg-opacity-50 h-100 d-none" style="z-index: 1100">
-    <div class="position-absolute top-50 start-50 translate-middle text-center">
-        <div class="spinner-border" role="status">
-        </div> 
-        <br>
-        <br>
-        <span>Caricamento</span>
-    </div>
-</div>
-
 <!-- Bootstrap JS -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
 
--- 1.2.9/utility/backend/body-start.php
+++ 1.2.9/utility/backend/body-start.php
@@ -1,2 +1,13 @@
+<!-- Progress bar -->
+<div id="loading-spinner" class="position-fixed container-fluid bg-dark bg-opacity-50 h-100 d-none" style="z-index: 1100">
+    <div class="position-absolute top-50 start-50 translate-middle text-center">
+        <div class="spinner-border" role="status">
+        </div> 
+        <br>
+        <br>
+        <span>Caricamento</span>
+    </div>
+</div>
+
 <!-- Custom .css - .js  -->
 <?php include $ROOT.'/custom/utility/backend/body-start.php';?>
