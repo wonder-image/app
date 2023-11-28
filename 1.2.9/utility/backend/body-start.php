9866335cc81398711cd9e01d8aadee0a1bc31b80
Andrea Marinoni <marinoni@wonderimage.it>
2023-11-28 11:43:37 +0100

    Loading spinner

--- 1.2.9/utility/backend/body-start.php
+++ 1.2.9/utility/backend/body-start.php
@@ -1,5 +1,5 @@
 <!-- Progress bar -->
-<div id="loading-spinner" class="position-fixed container-fluid bg-dark bg-opacity-50 h-100 d-none" style="z-index: 1100">
+<div id="loading-spinner" class="position-fixed container-fluid bg-dark bg-opacity-50 h-100 d-none top-0 start-0" style="z-index: 1100">
     <div class="position-absolute top-50 start-50 translate-middle text-center">
         <div class="spinner-border" role="status">
         </div> 
