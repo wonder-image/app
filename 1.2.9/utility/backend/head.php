9aa1c03d594489317630de4b17c9be2a77878826
Andrea Marinoni <marinoni@wonderimage.it>
2023-11-28 11:43:13 +0100

    Aggiornamento bootstrap 5.3.2

--- 1.2.9/utility/backend/head.php
+++ 1.2.9/utility/backend/head.php
@@ -15,7 +15,7 @@
 <link rel="shortcut icon" href="<?=$DEFAULT->BeFavicon?>" type="image/x-icon">
 
 <!-- Bootstrap CSS -->
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 
 <!-- Bootstrap Icons -->
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
