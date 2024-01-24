f02ded3d3e8c414fb91d20dc89d55bd9f61c04de
Andrea Marinoni <marinoni@wonderimage.it>
2024-01-24 17:39:15 +0100

    Possibilità di attivare le animazioni su mobile

--- 1.3.3/assets/js/frontend/aos.js
+++ 1.3.3/assets/js/frontend/aos.js
@@ -1,6 +1,6 @@
-if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) && (typeof MOBILE_REMOVE_AOS == 'undefined' || MOBILE_REMOVE_AOS == true)) {
     document.getElementById('aos-css').href = '';
     document.getElementById('aos-js').src = '';
-}else{
+} else {
     AOS.init();
 }
