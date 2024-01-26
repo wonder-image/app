4c6eefb73e80b74709830278e347fb8b9e55a6ed
Andrea Marinoni <marinoni@wonderimage.it>
2024-01-26 09:45:14 +0100

    Update aos.js

--- 1.3.3/assets/js/frontend/aos.js
+++ 1.3.3/assets/js/frontend/aos.js
@@ -1,4 +1,4 @@
-if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) && (typeof MOBILE_REMOVE_AOS == 'undefined' || MOBILE_REMOVE_AOS == true)) {
+if(isMobile() && (typeof MOBILE_REMOVE_AOS == 'undefined' || MOBILE_REMOVE_AOS == true)) {
     document.getElementById('aos-css').href = '';
     document.getElementById('aos-js').src = '';
 } else {
