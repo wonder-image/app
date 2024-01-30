58fe0fb798dbdccf3284934e4d98e2fed49be669
Andrea Marinoni <marinoni@wonderimage.it>
2024-01-30 15:03:36 +0100

    Update send.js

--- 1.3.3/assets/js/frontend/form/send.js
+++ 1.3.3/assets/js/frontend/form/send.js
@@ -102,3 +102,36 @@ function sendForm(button, url, responseFunction = prettyFormResponse) {
     });
 
 }
+
+function formUpload(form, url, responseFunction = prettyFormResponse) {
+
+    loadingSpinner();
+    
+    var formData = new FormData(form);
+    formData.append('post', 'true');
+    
+    $.ajax({
+        type: "POST",
+        url: url,
+        data: formData,
+        contentType: false,
+        cache: false,
+        processData:false, 
+        success: function (data) {
+
+            if (responseFunction != null) {
+                if (typeof responseFunction === 'function') {
+                    responseFunction(data);
+                } else {
+                    loadingSpinner();
+                }
+            }
+
+        },
+        error: function (XMLHttpRequest) {
+            ajaxRequestError(XMLHttpRequest);
+            loadingSpinner();
+        }
+    });
+
+}
