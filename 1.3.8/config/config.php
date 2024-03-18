1ab128b3491e64de8576fd57ac11b13d45a774b6
Andrea Marinoni <marinoni@wonderimage.it>
2024-03-18 18:22:28 +0100

    Impostazioni server email

--- 1.3.8/config/config.php
+++ 1.3.8/config/config.php
@@ -43,10 +43,10 @@
         #
 
     # Email
-        $MAIL->host = (isset($_ENV['MAIL_HOST']) && !empty($_ENV['MAIL_HOST'])) ? $_ENV['MAIL_HOST'] : "out.postassl.it";
-        $MAIL->username = (isset($_ENV['MAIL_USERNAME']) && !empty($_ENV['MAIL_USERNAME'])) ? $_ENV['MAIL_USERNAME'] : "noreply@wonderimage.it";
-        $MAIL->password = (isset($_ENV['MAIL_PASSWORD']) && !empty($_ENV['MAIL_PASSWORD'])) ? $_ENV['MAIL_PASSWORD'] : "PY@2PDY)h06";
-        $MAIL->port = (isset($_ENV['MAIL_PORT']) && !empty($_ENV['MAIL_PORT'])) ? $_ENV['MAIL_PORT'] : 465;
+        $MAIL->host = (isset($_ENV['MAIL_HOST']) && !empty($_ENV['MAIL_HOST'])) ? $_ENV['MAIL_HOST'] : "";
+        $MAIL->username = (isset($_ENV['MAIL_USERNAME']) && !empty($_ENV['MAIL_USERNAME'])) ? $_ENV['MAIL_USERNAME'] : "";
+        $MAIL->password = (isset($_ENV['MAIL_PASSWORD']) && !empty($_ENV['MAIL_PASSWORD'])) ? $_ENV['MAIL_PASSWORD'] : "";
+        $MAIL->port = (isset($_ENV['MAIL_PORT']) && !empty($_ENV['MAIL_PORT'])) ? $_ENV['MAIL_PORT'] : "";
         
     # Path
         $PATH->site = $_ENV['APP_URL'];
