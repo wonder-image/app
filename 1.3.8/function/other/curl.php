af8cb2eb9cd70ae2c4050d4ac9935aebab7dca83
Andrea Marinoni <marinoni@wonderimage.it>
2024-03-18 11:08:07 +0100

    Utilizzo nuove classi Api

--- 1.3.8/function/other/curl.php
+++ 1.3.8/function/other/curl.php
@@ -1,95 +1,31 @@
 <?php
 
-    function curl($url, $action = null, $values = null, $username = null, $password = null) {
+    function curl( string $url, string $action = null, array $values = [], string $username = null, string $password = null) {
 
-        $action = ($action == null) ? null : strtoupper($action);
+        $CALL = new Wonder/Api/Call($url, $values);
 
-        if ($action == 'GET' && is_array($values)) {
-            
-            $url .= "?".http_build_query($values);
+        $CALL->method($action);
 
+        if ($username != null && $password != null) {
+            $CALL->authBasic($username, $password);
         }
 
-        # Open cURL
-            $ch = curl_init();
-
-        # Connection
-            curl_setopt($ch, CURLOPT_URL, $url); 
-
-        # Get response
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-        # Post
-            if ($action == 'POST') {
-                curl_setopt($ch, CURLOPT_POST, true);
-                curl_setopt($ch, CURLOPT_POSTFIELDS, $values);
-            }
-        
-        # Authorise
-            if ($username != null && $password != null) {
-                curl_setopt($ch, CURLOPT_USERPWD, "$username:$password");
-            }
-
-        # Result
-            $result = curl_exec($ch);
-
-        # Control Error
-            if (curl_errno($ch)) {
-
-                $errno = curl_errno($ch); 
-                return curl_strerror($errno);
-                
-            } else {
-
-                return $result;
-
-            }
+        return $CALL->result();
 
     }
 
-    function wiApi($endpoint = null, array $values = []) {
+    function wiApi( string $endpoint, array $values = []) {
 
         global $API;
         
         $url = $API->endpoint.$endpoint;
 
-        # Codifico i valori in JSON
-            $values = json_encode($values);
+        $CALL = new Wonder/Api/Call($url, $values);
+        $CALL->method('POST');
+        $CALL->contentType('application/json');
+        $CALL->authBearer($API->key);
 
-        # Open cURL
-            $ch = curl_init();
-
-        # Connection
-            curl_setopt($ch, CURLOPT_URL, $url); 
-        
-        # Header
-            curl_setopt($ch, CURLOPT_HTTPHEADER, [
-                'Content-Type: application/json',
-                'Content-Length: '.strlen($values),
-                'Authorization: Bearer '.$API->key
-            ]);
-
-        # Get response
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-        # Post
-            curl_setopt($ch, CURLOPT_POST, true);
-            curl_setopt($ch, CURLOPT_POSTFIELDS, $values);
-
-        # Result
-            $result = curl_exec($ch);
-
-        # Control Error
-            if (curl_errno($ch)) {
-
-                $errno = curl_errno($ch); 
-                return curl_strerror($errno);
-                
-            } else {
-
-                return $result;
-
-            }
+        return $CALL->result();
 
     }
 
