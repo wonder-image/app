122b4b991f1f719103162069966d6dc39e67379a
Andrea Marinoni <marinoni@wonderimage.it>
2024-03-20 18:39:15 +0100

    Update Import.php

--- class/Plugin/Getrix/Import.php
+++ class/Plugin/Getrix/Import.php
@@ -6,13 +6,16 @@
 
     class Import {
 
-        private $endpoint = "https://feed.getrix.it/xml/";
-        public string $xml;
-        public object $array;
+        private $endpoint = [
+            'immobili' => 'https://feed.getrix.it/xml/',
+            'categorie' => 'https://feed.getrix.it/xml/tipologie.asp',
+            'comuni' => 'https://feed.getrix.it/xml/comuni.asp',
+            'quartieri' => 'https://feed.getrix.it/xml/quartieri.asp'
+        ];
 
-        function __construct($getrixId) {
+        public function Immobili($getrixId) {
 
-            $getrixFile = $this->endpoint.$getrixId.'.zip';
+            $getrixFile = $this->endpoint['immobili'].$getrixId.'.zip';
 
             $TMP_ZIP = $getrixId.'.zip';
             $TMP_XML = $getrixId.'.xml';
@@ -25,22 +28,47 @@
                 if ($unzipped === true) {
                     
                     # Estraggo il file zip
-                        $zip->extractTo(getcwd());
-                        $zip->close();
-
-                    # Trasformo il file XML in un'array
-                        $this->xml = $TMP_XML;
-                    
-                    # Trasformo il file XML in un'array
-                        $this->array = simplexml_load_file($this->xml)->children();
-                    
-                    # Elimino lo zip e xml
-                        unlink($TMP_ZIP);
-                        unlink($TMP_XML);
+                    $zip->extractTo(getcwd());
+                    $zip->close();
                         
+                    # Trasformo il file XML in un'array
+                    $array = simplexml_load_file($TMP_XML)->children();
+
+                    # Elimino lo zip e xml
+                    unlink($TMP_ZIP);
+                    unlink($TMP_XML);
+
+                    return $array;
+                        
+                } else {
+
+                    return [];
+
                 }
 
+            } else {
+
+                return [];
+
             }
+
+        }
+
+        public function Categorie() {
+
+            return json_decode(json_encode(simplexml_load_file($this->endpoint['categorie'])), true)['Categoria'];
+
+        }
+
+        public function Comuni() {
+
+            return json_decode(json_encode(simplexml_load_file($this->endpoint['comuni'])), true)['Comuni'];
+
+        }
+
+        public function Quartieri() {
+
+            return json_decode(json_encode(simplexml_load_file($this->endpoint['quartieri'])), true)['Quartiere'];
 
         }
 
