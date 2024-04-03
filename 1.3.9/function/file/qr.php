336d916bfe21a04c6de572646e49acc1326e6228
Andrea Marinoni <marinoni@wonderimage.it>
2024-04-03 14:54:17 +0200

    Update qr.php

--- 1.3.9/function/file/qr.php
+++ 1.3.9/function/file/qr.php
@@ -3,6 +3,14 @@
     function createQrCode($data, $src = null) {
 
         $options = new chillerlan\QRCode\QROptions;   
+
+        $options->version = 3;
+        $options->eccLevel = chillerlan\QRCode\Common\EccLevel::M;
+        $options->outputType = chillerlan\QRCode\Output\QROutputInterface::GDIMAGE_PNG;
+        $options->scale = 20;
+        $options->quality = 100;
+        $options->quietzoneSize = 2;
+        $options->outputBase64 = true;
         $options->imageTransparent = false;
 
         $qrcode = new chillerlan\QRCode\QRCode($options);
@@ -11,5 +19,3 @@
         return $qrcode;
 
     }
-
-?>
