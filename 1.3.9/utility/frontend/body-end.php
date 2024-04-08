ddf87fea11a49ada6f7ae7f8b1d3416ba75a9196
Andrea Marinoni <marinoni@wonderimage.it>
2024-04-08 10:57:31 +0200

    Update body-end.php

--- 1.3.9/utility/frontend/body-end.php
+++ 1.3.9/utility/frontend/body-end.php
@@ -1,9 +1,12 @@
 <section id="loading-spinner" class="p-f full-page bg-dark-0 d-none no-interaction" style="z-index: 1100">
     <div class="bg bg-dark-10 blur-2"></div>
     <div class="p-a center">
-        <div class="spinner-border" role="status">
-            <span class="visually-hidden">Loading...</span>
-        </div> 
+        <div class="title a-c">
+            <span class="spinner-border"></span> 
+        </div>
+        <div class="a-c text mt-4">
+            Loading...
+        </div>
     </div>
 </section>
 
