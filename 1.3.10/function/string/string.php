4a7e1978351ab39c48b0379f474e29c30071f305
Andrea Marinoni <marinoni@wonderimage.it>
2024-04-29 18:30:14 +0200

    Update string.php

--- 1.3.10/function/string/string.php
+++ 1.3.10/function/string/string.php
@@ -303,7 +303,9 @@
         $addressMore = empty($more) ? "" : "<br>$more";
         $addressMorePDF = empty($more) ? "" : "\n$more";
 
-        $RETURN->line = "$street $number, $cap $city ($province)";
+        $number = empty($number) ? "" : " $number";
+
+        $RETURN->line = "$street$number, $cap $city ($province)";
         $prettyPhone = empty($phone) ? "" : prettyPhone($phone);
 
         if (!empty($name) && !empty($surname) && !empty($phone)) {
@@ -311,36 +313,36 @@
             $RETURN->pretty = "
             <b>$name $surname</b><br>
             $prettyPhone<br>
-            $street $number, $cap <br>
+            $street$number, $cap <br>
             $city ($province)$addressMore";
 
-            $RETURN->prettyPDF = "$name $surname\n$prettyPhone\n$street $number, $cap\n$city ($province)$addressMorePDF";
+            $RETURN->prettyPDF = "$name $surname\n$prettyPhone\n$street$number, $cap\n$city ($province)$addressMorePDF";
 
         } else if (!empty($name) && !empty($surname)) {
 
             $RETURN->pretty = "
             <b>$name $RETURN->surname</b><br>
-            $street $number, $cap <br>
+            $street$number, $cap <br>
             $city ($province)$addressMore";
 
-            $RETURN->prettyPDF = "$name $surname\n$street $number, $cap\n$city ($province)$addressMore";
+            $RETURN->prettyPDF = "$name $surname\n$street$number, $cap\n$city ($province)$addressMore";
 
         } else if (!empty($name)) {
 
             $RETURN->pretty = "
             <b>$name</b><br>
-            $street $number, $cap <br>
+            $street$number, $cap <br>
             $city ($province)$addressMore";
 
-            $RETURN->prettyPDF = "$name\n$street $number, $cap\n$city ($province)$addressMore";
+            $RETURN->prettyPDF = "$name\n$street$number, $cap\n$city ($province)$addressMore";
 
         } else if (!empty($street) && !empty($number) && !empty($cap) && !empty($city) && !empty($province)) {
 
             $RETURN->pretty = "
-            $street $number, $cap <br>
+            $street$number, $cap <br>
             $city ($province)$addressMore";
 
-            $RETURN->prettyPDF = "$street $number, $cap\n$city ($province)$addressMore";
+            $RETURN->prettyPDF = "$street$number, $cap\n$city ($province)$addressMore";
 
         } else if (!empty($street) && !empty($cap) && !empty($city) && !empty($province)) {
             
