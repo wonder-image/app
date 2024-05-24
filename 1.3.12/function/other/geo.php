7ba3f998c596ca91d01021ad705ab01621ac305a
Andrea Marinoni <marinoni@wonderimage.it>
2024-05-24 18:12:46 +0200

    Nuove API geo

--- 1.3.12/function/other/geo.php
+++ 1.3.12/function/other/geo.php
@@ -1,98 +1,186 @@
 <?php
 
-    function geoContinent($KEY = "iso2", $VALUE = "name") {
-    
-        $JSON = file_get_contents("https://www.wonderimage.it/shared/ecommerce/v1.1/api/geo/continent.json");
-        $CONTINENT = json_decode($JSON, TRUE);
+    function country( $iso2 ) {
 
-        $RETURN = [];
+        $API = json_decode(wiApi('/service/csc/country/', [
+            'iso2' => $iso2
+        ]), true);
 
-        foreach ($CONTINENT as $key => $value) {
-            $RETURN[$value[$KEY]] = $value[$VALUE];
+        if ($API['success'] == true) {
+            return $API['response'];
+        } else {
+            return [];
         }
-
-        return $RETURN;
 
     }
 
-    function geoInfoContinent($VALUE, $KEY = 'iso2') {
+    function countries() {
 
-        $JSON = file_get_contents("https://www.wonderimage.it/shared/ecommerce/v1.1/api/geo/continent.json");
-        $CONTINENT = json_decode($JSON, TRUE);
+        $API = json_decode(wiApi('/service/csc/countries/'), true);
 
-        foreach ($CONTINENT as $key => $value) {
-            if ($value[$KEY] == $VALUE) {
-                $RETURN = $value;
+        if ($API['success'] == true) {
+
+            $COUNTRIES = [];
+            foreach ($API['response'] as $key => $value) { 
+
+                $name = empty($value['native']) ? $value['name'] : $value['native'];
+                $COUNTRIES[$value['iso2']] = $name; 
             }
-        }
 
-        return $RETURN;
+            # Ordine alfabetico
+            asort($COUNTRIES);
+
+            return $COUNTRIES;
+            
+        } else {
+            return [];
+        }
 
     }
 
-    function geoCountry($continent, $KEY = "iso2", $VALUE = "country-name") {
+    function state( $countryIso2, $stateIso2 ) {
 
-        $RETURN = [];
+        $API = json_decode(wiApi('/service/csc/state/', [
+            'country' => $countryIso2,
+            'state' => $stateIso2
+        ]), true);
 
-        if (is_array($continent)) {
-            foreach ($continent as $value) {
+        if ($API['success'] == true) {
+            return $API['response'];
+        } else {
+            return [];
+        }
 
-                $JSON = file_get_contents("https://www.wonderimage.it/shared/ecommerce/v1.1/api/geo/country/$value.json");
-                $COUNTRY = json_decode($JSON, TRUE);
+    }
 
-                foreach ($COUNTRY as $key => $value) {
-                    $RETURN[$KEY] = $value[$VALUE];
+
+    function states( $countryIso2 ) {
+
+        $API = json_decode(wiApi('/service/csc/states/', [
+            'country' => $countryIso2
+        ]), true);
+
+        if ($API['success'] == true) {
+
+            $STATES = [];
+
+            foreach ($API['response'] as $key => $value) { 
+
+                if (is_numeric($value['iso2'])) {
+
+                } else {
+                    $STATES[$value['iso2']] = $value['name']; 
                 }
 
             }
 
-        }else{
+            # Ordine alfabetico
+            asort($STATES);
 
-            $JSON = file_get_contents("https://www.wonderimage.it/shared/ecommerce/v1.1/api/geo/country/$continent.json");
-            $COUNTRY = json_decode($JSON, TRUE);
+            return $STATES;
 
-            foreach ($COUNTRY as $key => $value) {
+        } else {
+            return [];
+        }
+
+    }
+
+    // Vecchie funzioni 
+        function geoContinent($KEY = "iso2", $VALUE = "name") {
+        
+            $JSON = file_get_contents("https://www.wonderimage.it/shared/ecommerce/v1.1/api/geo/continent.json");
+            $CONTINENT = json_decode($JSON, TRUE);
+
+            $RETURN = [];
+
+            foreach ($CONTINENT as $key => $value) {
                 $RETURN[$value[$KEY]] = $value[$VALUE];
             }
 
+            return $RETURN;
+
         }
 
-        return $RETURN;
+        function geoInfoContinent($VALUE, $KEY = 'iso2') {
 
-    }
+            $JSON = file_get_contents("https://www.wonderimage.it/shared/ecommerce/v1.1/api/geo/continent.json");
+            $CONTINENT = json_decode($JSON, TRUE);
 
-    function geoInfoCountry($VALUE, $KEY = 'iso2') {
-
-        $CONTINENT = geoContinent();
-
-        foreach ($CONTINENT as $key => $value) {
-
-            $JSON = file_get_contents("https://www.wonderimage.it/shared/ecommerce/v1.1/api/geo/country/$key.json");
-            $COUNTRY = json_decode($JSON, TRUE);
-
-            foreach ($COUNTRY as $k => $v) {
-                if ($v[$KEY] == $VALUE) {
-                    $RETURN = $v;
+            foreach ($CONTINENT as $key => $value) {
+                if ($value[$KEY] == $VALUE) {
+                    $RETURN = $value;
                 }
             }
 
+            return $RETURN;
+
         }
 
-        return $RETURN;
+        function geoCountry($continent, $KEY = "iso2", $VALUE = "country-name") {
 
-    }
+            $RETURN = [];
 
-    function geoProvince($country, $KEY = "sigla", $VALUE = "nome") {
+            if (is_array($continent)) {
+                foreach ($continent as $value) {
 
-        $RETURN = [];
+                    $JSON = file_get_contents("https://www.wonderimage.it/shared/ecommerce/v1.1/api/geo/country/$value.json");
+                    $COUNTRY = json_decode($JSON, TRUE);
 
-        $JSON = file_get_contents("https://www.wonderimage.it/shared/ecommerce/v1.1/api/geo/province/$country.json");
-        $PROVINCE = json_decode($JSON, TRUE);
+                    foreach ($COUNTRY as $key => $value) {
+                        $RETURN[$KEY] = $value[$VALUE];
+                    }
 
-        foreach ($PROVINCE as $key => $value) {
-            $RETURN[$value[$KEY]] = $value[$VALUE];
+                }
+
+            }else{
+
+                $JSON = file_get_contents("https://www.wonderimage.it/shared/ecommerce/v1.1/api/geo/country/$continent.json");
+                $COUNTRY = json_decode($JSON, TRUE);
+
+                foreach ($COUNTRY as $key => $value) {
+                    $RETURN[$value[$KEY]] = $value[$VALUE];
+                }
+
+            }
+
+            return $RETURN;
+
         }
 
-        return $RETURN;
+        function geoInfoCountry($VALUE, $KEY = 'iso2') {
 
-    }
+            $CONTINENT = geoContinent();
+
+            foreach ($CONTINENT as $key => $value) {
+
+                $JSON = file_get_contents("https://www.wonderimage.it/shared/ecommerce/v1.1/api/geo/country/$key.json");
+                $COUNTRY = json_decode($JSON, TRUE);
+
+                foreach ($COUNTRY as $k => $v) {
+                    if ($v[$KEY] == $VALUE) {
+                        $RETURN = $v;
+                    }
+                }
+
+            }
+
+            return $RETURN;
+
+        }
+
+        function geoProvince($country, $KEY = "sigla", $VALUE = "nome") {
+
+            $RETURN = [];
+
+            $JSON = file_get_contents("https://www.wonderimage.it/shared/ecommerce/v1.1/api/geo/province/$country.json");
+            $PROVINCE = json_decode($JSON, TRUE);
+
+            foreach ($PROVINCE as $key => $value) {
+                $RETURN[$value[$KEY]] = $value[$VALUE];
+            }
+
+            return $RETURN;
+
+        }
+
+    // 
