d2fbe4ed87a504a72329fea87e73e5c7aac9bead
Andrea Marinoni <marinoni@wonderimage.it>
2024-06-12 17:21:31 +0200

    Update connection.php

--- 1.3.14/database/connection.php
+++ 1.3.14/database/connection.php
@@ -7,7 +7,8 @@
     foreach ($DB->database as $key => $database) {
         
         $connection = new Connection( $DB->hostname, $DB->username, $DB->password, $database );
-        $MYSQLI_CONNECTION[$key] = $connection->Connect();
+        $MYSQLI_CONNECTION[$key] = $connection->Connect(); # Nome associato al database
+        $MYSQLI_CONNECTION[$database] = $connection->Connect(); # Vero nome database
 
     }
 
