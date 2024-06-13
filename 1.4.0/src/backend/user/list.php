87a2618dcac39fadec58738f2888e8a05e446e6a
Andrea Marinoni <marinoni@wonderimage.it>
2024-06-13 12:30:46 +0200

    Update list.php

--- 1.4.0/src/backend/user/list.php
+++ 1.4.0/src/backend/user/list.php
@@ -2,12 +2,10 @@
 
     $BACKEND = true;
     $PRIVATE = true;
-    $PERMIT = ['admin'];
+    $PERMIT = [];
 
     $ROOT = $_SERVER['DOCUMENT_ROOT'];
     require_once $ROOT."/vendor/wonder-image/app/wonder-image.php";
 
     require_once "set-up.php";
     require_once $ROOT_APP."/html/backend/list.php";
-    
-?>
