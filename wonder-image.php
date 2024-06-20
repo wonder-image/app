07394f028ea32d86e7c489ba019e00249de433eb
Andrea Marinoni <marinoni@wonderimage.it>
2024-06-20 10:29:55 +0200

    Update wonder-image.php

--- wonder-image.php
+++ wonder-image.php
@@ -20,7 +20,7 @@
         if (!isset($_SESSION['user_id'])) { $_SESSION['user_id'] = null; }
         
     $APP_VERSION = "1.4.0";
-    $LIB_VERSION = "1.5.1";
+    $LIB_VERSION = "1.5.3";
     $ROOT_APP = __DIR__."/$APP_VERSION";
 
     require_once $ROOT."/vendor/autoload.php";
