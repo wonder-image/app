36e40e38a325ff49bdf8bf5f75f5b291cf2c7b50
Andrea Marinoni <marinoni@wonderimage.it>
2024-07-05 10:31:25 +0200

    Nuovo font FPDF - Montserrat

--- 1.4.0/config/fpdf.php
+++ 1.4.0/config/fpdf.php
@@ -3,17 +3,46 @@
     # Per aggiungere font guarda nella cartella assets/font/README.md
 
     $FONT_FPDF = [
+
+        # Default FPDF
         'helvetica' => 'Arial',
         'courier' => 'Courier',
         'symbol' => 'Symbol',
         'times' => 'Times',
+
+        # Nunito Sans
         'NunitoSans-Light' => 'NunitoSans Light',
         'NunitoSans-Regular' => 'NunitoSans Regular',
         'NunitoSans-Medium' => 'NunitoSans Medium',
         'NunitoSans-Bold' => 'NunitoSans Bold',
+
+        # EB Garamond
         'EBGaramond-Regular' => 'EB Garamond Regular',
         'EBGaramond-Medium' => 'EB Garamond Medium',
         'EBGaramond-SemiBold' => 'EB Garamond SemiBold',
         'EBGaramond-Bold' => 'EB Garamond Bold',
         'EBGaramond-ExtraBold' => 'EB Garamond ExtraBold',
+
+        # Montserrat
+        'Montserrat-Thin' => 'Montserrat Thin',
+        'Montserrat-ExtraLight' => 'Montserrat Extra Light',
+        'Montserrat-Light' => 'Montserrat Light',
+        'Montserrat-Regular' => 'Montserrat Regular',
+        'Montserrat-Medium' => 'Montserrat Medium',
+        'Montserrat-SemiBold' => 'Montserrat Semi Bold',
+        'Montserrat-Bold' => 'Montserrat Bold',
+        'Montserrat-ExtraBold' => 'Montserrat Extra Bold',
+        'Montserrat-Black' => 'Montserrat Black',
+
+        # Montserrat Italic
+        'Montserrat-ThinItalic' => 'Montserrat Thin Italic',
+        'Montserrat-ExtraLightItalic' => 'Montserrat Extra Light Italic',
+        'Montserrat-LightItalic' => 'Montserrat Light Italic',
+        'Montserrat-MediumItalic' => 'Montserrat Medium Italic',
+        'Montserrat-RegularItalic' => 'Montserrat Regular Italic',
+        'Montserrat-SemiBoldItalic' => 'Montserrat Semi Bold Italic',
+        'Montserrat-BoldItalic' => 'Montserrat Bold Italic',
+        'Montserrat-ExtraBoldItalic' => 'Montserrat Extra Bold Italic',
+        'Montserrat-BlackItalic' => 'Montserrat Black Italic',
+        
     ];
