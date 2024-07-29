7412f99bca6d34523324d9243aaac6fbe18ea4fa
Andrea Marinoni <marinoni@wonderimage.it>
2024-07-29 12:11:49 +0200

    Update update.php

--- 1.4.1/build/page/update.php
+++ 1.4.1/build/page/update.php
@@ -4,5 +4,3 @@
     if (is_dir($ROOT.'/assets/upload/user/profile-picture/') === false) { mkdir($ROOT.'/assets/upload/user/profile-picture/'); }
 
     if (file_exists($ROOT.'/update/page/index.php')) { deleteDir($ROOT.'/update/'); }
-
-    copyDir($ROOT_APP.'/src/update/', $ROOT.'/update/');
