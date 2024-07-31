9d357b227e0e6226ba2769bec9428e5d5ab1e716
Andrea Marinoni <marinoni@wonderimage.it>
2024-07-31 17:02:30 +0200

    Update Connection.php

--- class/Sql/Connection.php
+++ class/Sql/Connection.php
@@ -2,44 +2,64 @@
 
     namespace Wonder\Sql;
 
+    use Wonder\App\Credentials;
+    
     use mysqli;
     use Exception;
 
     class Connection {
 
-        private $hostname, $username, $password, $database;
-        
-        public function __construct( $hostname, $username, $password, $database ) {
+        private static $hostname, $username, $password, $database;
 
-            $this->hostname = $hostname;
-            $this->username = $username;
-            $this->password = $password;
-            $this->database = $database;
+        private static $connection = [];
+
+        
+        public function __construct( $hostname = null, $username = null, $password = null, $database = null ) 
+        {
+
+            self::$hostname = ($hostname === null) ? Credentials::database()->hostname : $hostname;
+            self::$username = ($username === null) ? Credentials::database()->username : $username;
+            self::$password = ($password === null) ? Credentials::database()->password : $password;
+            self::$database = ($database === null) ? Credentials::database()->database['main'] : $database;
 
         }
 
-        public function Connect() : mysqli 
+        public static function Connect( $database = null): mysqli 
         { 
 
-            $mysqli = new mysqli( $this->hostname, $this->username, $this->password, $this->database );
-                    
-            if ($mysqli->connect_error) {
-                
-                $message = "\r\n";
-                $message .= "Connessione al datatbase $this->database fallita \r\n";
-                $message .= "\r\n";
-                $message .= "Error N°$mysqli->connect_errno\r\n";
-                $message .= "$mysqli->connect_error\r\n";
-    
-                throw new Exception($message);
+            $database = ($database === null) ? self::$database : $database;
+
+            $connectionCode = base64_encode(self::$hostname.' | '.self::$username.' | '.self::$password.' | '.$database);
+
+            if (isset(self::$connection[$connectionCode])) {
+
+                return self::$connection[$connectionCode];
 
             } else {
 
-                $mysqli->set_charset("latin1");
+                $mysqli = new mysqli( self::$hostname, self::$username, self::$password, $database );
+                        
+                if ($mysqli->connect_error) {
+                    
+                    $message = "\r\n";
+                    $message .= "Connessione al database ".self::$database." fallita \r\n";
+                    $message .= "\r\n";
+                    $message .= "Error N°$mysqli->connect_errno\r\n";
+                    $message .= "$mysqli->connect_error\r\n";
+        
+                    throw new Exception($message);
+
+                } else {
+
+                    $mysqli->set_charset("latin1");
+
+                }
+
+                self::$connection[$connectionCode] = $mysqli;
+
+                return $mysqli;
 
             }
-
-            return $mysqli;
 
         }
 
