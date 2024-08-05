429695c82c564aae355cb8bca614588f8dd29a39
Andrea Marinoni <marinoni@wonderimage.it>
2024-08-05 18:21:59 +0200

    Update middleware.php

--- 1.4.1/middleware/middleware.php
+++ 1.4.1/middleware/middleware.php
@@ -1,6 +1,6 @@
 <?php
 
-    require_once $ROOT_APP."/middleware/auth.php";
     require_once $ROOT_APP."/middleware/error.php";
     require_once $ROOT_APP."/middleware/update.php";
+    require_once $ROOT_APP."/middleware/auth.php";
     
