b76e427a9bcab8d2d128c4df637c0f07e2104cb3
Andrea Marinoni <marinoni@wonderimage.it>
2024-09-07 10:39:34 +0200

    Cambio funzione scandir

--- 1.4.1/config/app/table.php
+++ 1.4.1/config/app/table.php
@@ -1,21 +1,17 @@
 <?php
 
-    $files = empty(scandir("$ROOT_APP/build/table/")) ? [] : scandir("$ROOT_APP/build/table/");
+    $files = scanParentDir("$ROOT_APP/build/table/");
 
     foreach ($files as $file) {
-        if ($file != '' && $file != '.' && $file != '..') {
-            if (isset(pathinfo($file)['extension']) && pathinfo($file)['extension'] == 'php') {
-                require_once "$ROOT_APP/build/table/$file";
-            }
+        if (isset(pathinfo($file)['extension']) && pathinfo($file)['extension'] == 'php') {
+            require_once "$ROOT_APP/build/table/$file";
         }
     }
 
-    $files = empty(scandir("$ROOT/custom/build/table/")) ? [] : scandir("$ROOT/custom/build/table/");
+    $files = scanParentDir("$ROOT/custom/build/table/");
 
     foreach ($files as $file) {
-        if ($file != '' && $file != '.' && $file != '..') {
-            if (isset(pathinfo($file)['extension']) && pathinfo($file)['extension'] == 'php') {
-                require_once "$ROOT/custom/build/table/$file";
-            }
+        if (isset(pathinfo($file)['extension']) && pathinfo($file)['extension'] == 'php') {
+            require_once "$ROOT/custom/build/table/$file";
         }
     }
