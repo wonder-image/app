798940dee1aaddb6947a111074b5ce6496fc1e6a
Andrea Marinoni <marinoni@wonderimage.it>
2024-09-20 15:08:12 +0200

    Update config.php

--- 1.4.2/config/config.php
+++ 1.4.2/config/config.php
@@ -1,7 +1,7 @@
 <?php
 
     require_once __DIR__."/array/array.php";
+    require_once __DIR__."/style/style.php";
     require_once __DIR__."/connection/connection.php";
     require_once __DIR__."/app/app.php";
-    require_once __DIR__."/style/style.php";
     require_once $ROOT."/custom/config/config.php"; # Configurazioni CUSTOM
