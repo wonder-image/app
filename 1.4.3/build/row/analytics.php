99111c48687fbebac854736fec1d08eeead3bc0e
Andrea Marinoni <marinoni@wonderimage.it>
2025-01-22 22:22:54 +0100

    Update analytics.php

--- 1.4.3/build/row/analytics.php
+++ 1.4.3/build/row/analytics.php
@@ -15,11 +15,28 @@
 
 
     if (!sqlSelect('security', ['id' => 1], 1)->exists) {
-            
+
         $values = [
-            "api_key" => $API->key
+            "api_key" => $API->key,
+            "gcp_project_id" => $API->gcp_project_id,
+            "gcp_api_key" => $API->gcp_api_key
         ];
 
         sqlInsert('security', $values);
+
+    } else {
+
+        $row = sqlSelect('security', [ 'id' => 1 ], 1)->row;
+
+        if (empty($row['gcp_project_id']) || empty($row['gcp_api_key'])) {
+                        
+            $values = [
+                "gcp_project_id" => $API->gcp_project_id,
+                "gcp_api_key" => $API->gcp_api_key
+            ];
+    
+            sqlModify('security', $values, 'id', 1);
+
+        }
 
     }
