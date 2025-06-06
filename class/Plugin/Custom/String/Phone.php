113b378522cf9d20767236f7356932932e35758c
Andrea Marinoni <marinoni@wonderimage.it>
2025-06-06 18:26:32 +0200

    Update Phone.php

--- class/Plugin/Custom/String/Phone.php
+++ class/Plugin/Custom/String/Phone.php
@@ -4,6 +4,34 @@
 
     class Phone {
 
+        public static string $defaultCountryCode = '+39';
+
+        public static function format($number) 
+        {
+
+            // Rimuove spazi, punti e trattini
+            $number = preg_replace('/[\s\.\-]/', '', $number);
+
+            // Se inizia con +, consideriamo già in formato internazionale
+            if (str_starts_with($number, '+')) {
+                return $number;
+            }
+
+            // Se inizia con 00, lo convertiamo in +
+            if (str_starts_with($number, '00')) {
+                return '+' . substr($number, 2);
+            }
+
+            // Se inizia con 0, lo trattiamo come numero nazionale italiano
+            if (str_starts_with($number, '0')) {
+                return self::$defaultCountryCode . $number;
+            }
+
+            // Se arriva qui potrebbe essere un numero senza prefisso
+            return self::$defaultCountryCode . $number;
+
+        }
+
         public static function analyze($number) {
 
             $RETURN = (object) [];
@@ -13,7 +41,7 @@
 
             if (!empty($number)) {
 
-                $number = str_replace(' ', '', $number);
+                $number = self::format($number);
 
                 $RETURN = (object) [];
                 $RETURN->prefix = '';
@@ -39,6 +67,7 @@
                 $analyze = self::analyze($number);
 
                 $number = '';
+                
                 if (strlen($analyze->number) <= 4) {
                    $number = $analyze->number;
                 } else if (substr($analyze->number, 0, 1) == '0') {
