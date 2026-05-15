725f117d5327a3802f0642a385e6d2a3a03caef1
Andrea Marinoni <marinoni@wonderimage.it>
2026-05-15 14:30:43 +0200

    Update base.php

--- app/view/layout/frontend/base.php
+++ app/view/layout/frontend/base.php
@@ -2,16 +2,15 @@
 <html lang="<?=__l()?>">
 <head>
 
-    <?php  include $ROOT_APP.'/utility/frontend/head.php'; ?>
+    <?php include $ROOT_APP.'/utility/frontend/head.php'; ?>
 
 </head>
 <body>
 
     <?php include $ROOT_APP.'/utility/frontend/body-start.php' ?>
-    <?php include $ROOT.'/custom/utility/frontend/header.php' ?>
 
+    <?=$PAGE_CONTENT?>
     
-    <?php include $ROOT.'/custom/utility/frontend/footer.php' ?>
     <?php include $ROOT_APP.'/utility/frontend/body-end.php' ?>
     
 </body>
