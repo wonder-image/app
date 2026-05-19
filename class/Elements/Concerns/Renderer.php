c2ec3df78f7409c5179cc4e92b5ac02a811d7de5
Andrea Marinoni <marinoni@wonderimage.it>
2026-05-19 11:52:40 +0200

    Update Renderer.php

--- class/Elements/Concerns/Renderer.php
+++ class/Elements/Concerns/Renderer.php
@@ -9,6 +9,14 @@ trait Renderer
     /**
      * Renderizza l'elemento col tema attivo o con un tema esplicito.
      */
+
+    public function __tostring()
+    {
+
+        return $this->render();
+
+    }
+    
     public function render(?string $theme = null): string
     {
         $renderer = Resolver::renderer(static::class, $theme);
